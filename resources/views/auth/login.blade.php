9bbe5164dacb93314a4efa5b41c2cc1135bf5567
Bernard Jelinic <jelinic.bernard@gmail.com>
2023-11-03 13:25:48 +0100

    Create login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,185 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layouts.app')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('content')
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <div class="sign-in-page">
+        <div class="signin-popup">
+            <div class="signin-pop">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="cmp-info">
+                            <div class="cm-logo">
+                                <!--<img src="{{ asset('assets/images/remote-connections_3.jpg') }}" alt="Application Logo">-->
+                                <p>Connect with friends and the world around you.</p>
+                            </div><!--cm-logo end-->
+                            <img src="{{asset('assets/images/cm-main-img.png') }}" alt="">
+                        </div><!--cmp-info end-->
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="login-sec">
+                            <ul class="sign-control">
+                                <li data-tab="tab-1" class="current"><a href="#" title="">Sign in</a></li>
+                                <li data-tab="tab-2"><a href="#" title="">Sign up</a></li>
+                            </ul>
+                            <div class="sign_in_sec current" id="tab-1">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+                                <h3>Sign in</h3>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-lg-12 no-pdd">
+                                            <div class="sn-field">
+                                                <input type="email" name="email" placeholder="Email" :value="old('email')" required autofocus autocomplete="username">
+                                                <i class="la la-envelope"></i>
+                                            </div><!--sn-field end-->
+                                        </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+                                        <div class="col-lg-12 no-pdd">
+                                            <div class="sn-field">
+                                                <input id="password" type="password" name="password" placeholder="Password" required autocomplete="current-password">
+                                                <i class="la la-lock"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 no-pdd">
+                                            <div class="checky-sec">
+                                                <!-- Remember Me start -->
+                                                <div class="fgt-sec">
+                                                    <input type="checkbox" name="remember" id="remember_me">
+                                                    <label for="remember_me">
+                                                        <span></span>
+                                                    </label>
+                                                    <small>Remember me</small>
+                                                </div><!--fgt-sec end-->
+                                                <!-- Remember Me end -->
+                                                <a href="{{ route('password.request') }}" title="">Forgot Password?</a>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 no-pdd">
+                                            <button type="submit" value="submit">Sign in</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div><!--sign_in_sec end-->
+                            <div class="sign_in_sec" id="tab-2">
+                                <div class="signup-tab">
+                                    <i class="fa fa-long-arrow-left"></i>
+                                    <h2>johndoe@example.com</h2>
+                                    <ul>
+                                        <li data-tab="tab-3" class="current"><a href="#" title="">User</a></li>
+                                        <li data-tab="tab-4"><a href="#" title="">Company</a></li>
+                                    </ul>
+                                </div><!--signup-tab end-->
+                                <div class="dff-tab current" id="tab-3">
+                                    <form method="POST" action="{{ route('register') }}">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="text" id="name" name="name" placeholder="Full Name" :value="old('name')" required autofocus autocomplete="name">
+                                                    <i class="la la-user"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="email" id="email" name="email" placeholder="Email" :value="old('email')" required autofocus autocomplete="username">
+                                                    <i class="la la-envelope"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="text" name="country" placeholder="Country">
+                                                    <i class="la la-globe"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <select>
+                                                        <option>Category</option>
+                                                    </select>
+                                                    <i class="la la-dropbox"></i>
+                                                    <span><i class="fa fa-ellipsis-h"></i></span>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="password" id="password" name="password" placeholder="Password" required autocomplete="new-password">
+                                                    <i class="la la-lock"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="password" id="password_confirmation" name="password_confirmation"
+                                                        placeholder="Repeat Password" required autocomplete="new-password">
+                                                    <i class="la la-lock"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <button type="submit" value="submit">Get Started</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div><!--dff-tab end-->
+                                <div class="dff-tab" id="tab-4">
+                                    <form method="POST" action="{{ route('register') }}">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="text" id="name" name="name" placeholder="Full Name" :value="old('name')" required autofocus autocomplete="name">
+                                                    <i class="la la-user"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="text" name="company-name"
+                                                        placeholder="Company Name">
+                                                    <i class="la la-building"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="text" name="country" placeholder="Country">
+                                                    <i class="la la-globe"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="password" id="password" name="password" placeholder="Password" required autocomplete="new-password">
+                                                    <i class="la la-lock"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <div class="sn-field">
+                                                    <input type="password" id="password_confirmation" name="password_confirmation"
+                                                        placeholder="Repeat Password" required autocomplete="new-password">
+                                                    <i class="la la-lock"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 no-pdd">
+                                                <button type="submit" value="submit">Get Started</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div><!--dff-tab end-->
+                            </div>
+                        </div><!--login-sec end-->
+                    </div>
+                </div>
+            </div><!--signin-pop end-->
+        </div><!--signin-popup end-->
+        <div class="footy-sec">
+            <div class="container">
+                <!--<ul>
+                    <li><a href="#" title="">Help Center</a></li>
+                    <li><a href="#" title="">Privacy Policy</a></li>
+                    <li><a href="#" title="">Community Guidelines</a></li>
+                    <li><a href="#" title="">Cookies Policy</a></li>
+                    <li><a href="#" title="">Career</a></li>
+                    <li><a href="#" title="">Forum</a></li>
+                    <li><a href="#" title="">Language</a></li>
+                    <li><a href="#" title="">Copyright Policy</a></li>
+                </ul>-->
+                <p><img src="{{ asset('assets/images/copy-icon.png') }}" alt="">Copyright <?php echo date('Y'); ?></p>
+            </div>
+        </div><!--footy-sec end-->
+    </div><!--sign-in-page end-->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,31 +6,31 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/animate.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/line-awesome.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/line-awesome-font-awesome.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/lib/slick/slick.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/lib/slick/slick-theme.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css') }}">
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <body class="{{ Route::is('login') ? 'sign-in' : '' }}">
+        <!--theme-layout start-->
+        <div class="wrapper">
+            @yield('content')
         </div>
+        <!--theme-layout end-->
     </body>
+
+    <script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/popper.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/lib/slick/slick.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/script.js') }}"></script>
 </html>
