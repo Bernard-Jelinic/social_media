33ed88c3e2e24d872749f61a768c31b869655fdb
Bernard Jelinic <jelinic.bernard@gmail.com>
2023-12-08 14:18:08 +0100

    Write test for livewire FriendManagementComponent methods for addFriend and cancelFriendRequest

---
+++ tests/Feature/Livewire/FriendManagementTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use Tests\TestCase;
+use App\Models\User;
+use Livewire\Livewire;
+use App\Livewire\FriendManagementComponent;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FriendManagementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_can_add_friend(): void
+    {
+        // // Create two user
+        $user = User::factory()->create();
+        $another_user = User::factory()->create();
+
+        // // Acting as the authenticated user
+        $this->actingAs($user);
+
+        // // Livewire test
+        Livewire::test(FriendManagementComponent::class, ['user_profile' => $another_user])
+            ->call('addFriend');
+
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function user_can_cancel_friend_request(): void
+    {
+        // // Create two user
+        $user = User::factory()->create();
+        $another_user = User::factory()->create();
+
+        // // Acting as the authenticated user
+        $this->actingAs($user);
+
+        // // first you need to add friend to have option to cancel friend request
+        Livewire::test(FriendManagementComponent::class, ['user_profile' => $another_user])
+            ->call('addFriend');
+
+        // // Livewire test
+        Livewire::test(FriendManagementComponent::class, ['user_profile' => $another_user])
+            ->call('cancelFriendRequest');
+
+        $this->assertTrue(true);
+    }
+}
