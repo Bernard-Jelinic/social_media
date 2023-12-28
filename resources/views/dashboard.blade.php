10682589381f3036d8032749262c6862ff78d254
Bernard Jelinic <jelinic.bernard@gmail.com>
2023-12-28 08:55:23 +0100

    Delete unnecessary code on dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -135,18 +135,9 @@
                     </div>
                     <div class="col-lg-3 pd-right-none no-pd">
                         <div class="right-sidebar">
-                            <div class="widget widget-about">
-                                <img src="{{asset('assets/images/wd-logo.png') }}" alt="">
-                                <h3>Track Time on Workwise</h3>
-                                <span>Pay only for the Hours worked</span>
-                                <div class="sign_link">
-                                    <h3><a href="#" title="">Sign up</a></h3>
-                                    <a href="#" title="">Learn More</a>
-                                </div>
-                            </div><!--widget-about end-->
                             <div class="widget widget-jobs">
                                 <div class="sd-title">
-                                    <h3>Top Jobs</h3>
+                                    <h3>Top Pages</h3>
                                     <i class="la la-ellipsis-v"></i>
                                 </div>
                                 <div class="jobs-list">
@@ -232,65 +223,6 @@
                                     </div><!--job-info end-->
                                 </div><!--jobs-list end-->
                             </div><!--widget-jobs end-->
-                            <div class="widget suggestions full-width">
-                                <div class="sd-title">
-                                    <h3>Most Viewed People</h3>
-                                    <i class="la la-ellipsis-v"></i>
-                                </div><!--sd-title end-->
-                                <div class="suggestions-list">
-                                    <div class="suggestion-usd">
-                                        <img src="http://via.placeholder.com/35x35" alt="">
-                                        <div class="sgt-text">
-                                            <h4>Jessica William</h4>
-                                            <span>Graphic Designer</span>
-                                        </div>
-                                        <span><i class="la la-plus"></i></span>
-                                    </div>
-                                    <div class="suggestion-usd">
-                                        <img src="http://via.placeholder.com/35x35" alt="">
-                                        <div class="sgt-text">
-                                            <h4>John Doe</h4>
-                                            <span>PHP Developer</span>
-                                        </div>
-                                        <span><i class="la la-plus"></i></span>
-                                    </div>
-                                    <div class="suggestion-usd">
-                                        <img src="http://via.placeholder.com/35x35" alt="">
-                                        <div class="sgt-text">
-                                            <h4>Poonam</h4>
-                                            <span>Wordpress Developer</span>
-                                        </div>
-                                        <span><i class="la la-plus"></i></span>
-                                    </div>
-                                    <div class="suggestion-usd">
-                                        <img src="http://via.placeholder.com/35x35" alt="">
-                                        <div class="sgt-text">
-                                            <h4>Bill Gates</h4>
-                                            <span>C &amp; C++ Developer</span>
-                                        </div>
-                                        <span><i class="la la-plus"></i></span>
-                                    </div>
-                                    <div class="suggestion-usd">
-                                        <img src="http://via.placeholder.com/35x35" alt="">
-                                        <div class="sgt-text">
-                                            <h4>Jessica William</h4>
-                                            <span>Graphic Designer</span>
-                                        </div>
-                                        <span><i class="la la-plus"></i></span>
-                                    </div>
-                                    <div class="suggestion-usd">
-                                        <img src="http://via.placeholder.com/35x35" alt="">
-                                        <div class="sgt-text">
-                                            <h4>John Doe</h4>
-                                            <span>PHP Developer</span>
-                                        </div>
-                                        <span><i class="la la-plus"></i></span>
-                                    </div>
-                                    <div class="view-more">
-                                        <a href="#" title="">View More</a>
-                                    </div>
-                                </div><!--suggestions-list end-->
-                            </div>
                         </div><!--right-sidebar end-->
                     </div>
                 </div>
