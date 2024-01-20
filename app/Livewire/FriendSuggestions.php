375f8aece1c6f6c89ec487a2452cd2c016ad393e
Bernard Jelinic <jelinic.bernard@gmail.com>
2024-01-20 08:22:34 +0100

    Create reusable users-sidebar livewire component

--- app/Livewire/FriendSuggestions.php
+++ app/Livewire/FriendSuggestions.php
@@ -7,7 +7,8 @@ use App\Livewire\FriendBaseComponent;
 
 class FriendSuggestions extends FriendBaseComponent
 {
-    public $suggestions;
+    public $users;
+    public $title;
 
     public function mount(): void
     {
@@ -17,7 +18,7 @@ class FriendSuggestions extends FriendBaseComponent
     public function get(): void
     {
         $currentUser = auth()->user();
-        $this->suggestions = User::whereNotIn('id', function ($query) use ($currentUser) {
+        $this->users = User::whereNotIn('id', function ($query) use ($currentUser) {
             $query->select('sender_id')
                 ->from('friends')
                 ->where('receiver_id', $currentUser->id);
@@ -32,10 +33,11 @@ class FriendSuggestions extends FriendBaseComponent
             ->inRandomOrder()
             ->take(6)
             ->get();
+        $this->title = 'Suggestions';
     }
 
     public function render()
     {
-        return view('livewire.friend-suggestions');
+        return view('livewire.shared.pages.users-sidebar');
     }
 }
---
+++ resources/views/livewire/shared/pages/users-sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="users full-width">
+    <div class="sd-title">
+        <h3>{{ $title }}</h3>
+    </div><!--sd-title end-->
+    <div class="users-list">
+        @foreach ($users as $user)
+            <div class="suggestion-usd">
+                <img style="width: 35px;" src="{{ asset($user->profile_image) }}" alt="Profile image">
+                <div class="sgt-text">
+                    <h4>{{ $user->full_name }}</h4>
+                    <span>{{ $user->headline }}</span>
+                </div>
+                <span wire:click="addFriendAndRefresh({{ $user }})"><i class="la la-plus"></i></span>
+            </div>    
+        @endforeach
+    </div><!--users-list end-->
+</div><!--users end-->
