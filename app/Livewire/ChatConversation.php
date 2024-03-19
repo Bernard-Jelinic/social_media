ac63d885bdf4ca60ca4223622e2332811e3e6b4f
Bernard Jelinic <jelinic.bernard@gmail.com>
2024-03-19 18:01:35 +0100

    Fix opening conversation that don't have message

--- app/Livewire/ChatConversation.php
+++ app/Livewire/ChatConversation.php
@@ -3,8 +3,10 @@
 namespace App\Livewire;
 
 use Musonza\Chat\Models\Conversation;
+use App\Models\User;
 use Livewire\Component;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ChatConversation extends Component
 {
@@ -15,10 +17,12 @@ class ChatConversation extends Component
     public function mount(Request $request): void
     {
 
+        // // if in url exist conversation_id
         if ($request->route('conversation_id')) {
             $conversation = Conversation::find($request->route('conversation_id'));
             $this->is_message_exist = true;
         } else {
+            // // if in url don't exist conversation_id
             $participantId = auth()->user()->id;
 
             $conversation = Conversation::whereHas('messages', function ($query) use ($participantId) {
@@ -26,7 +30,6 @@ class ChatConversation extends Component
             })->latest('updated_at')->first();
     
             if ($conversation) {
-                $messages = $conversation->messages()->orderBy('created_at', 'asc')->get();
                 $conversation = Conversation::find($conversation->id);
                 // Process messages as shown previously
                 $this->is_message_exist = true;
@@ -37,11 +40,21 @@ class ChatConversation extends Component
 
         if ($this->is_message_exist) {
             $this->selected_conversation = $conversation->messages()->orderBy('created_at', 'asc')->get();
-    
-            foreach ($this->selected_conversation as $conversation) {
-                if ( auth()->user()->id !== $conversation->id ) {
-                    $this->selected_participant = \App\Models\User::find($conversation->id);
+
+            if (count($this->selected_conversation)) {
+                for ($i=0; $i < count($this->selected_conversation); $i++) { 
+                    $conversation = $this->selected_conversation[$i];
+                    if ( auth()->user()->id !== $conversation->id ) {
+                        $this->selected_participant = User::find($conversation->id);
+                        $i = count($this->selected_conversation);
+                    }
                 }
+            } else {
+                $conversations = DB::table('chat_participation')
+                                    ->where('conversation_id', $request->route('conversation_id'))
+                                    ->where('messageable_id', '!=', auth()->id())
+                                    ->get();
+                $this->selected_participant = User::find($conversations[0]->messageable_id);
             }
         }
     }
