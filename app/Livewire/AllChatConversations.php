378e61e1d2d036e82a9f96876d6db32eb271245d
Bernard Jelinic <jelinic.bernard@gmail.com>
2024-04-13 14:03:25 +0200

    Display all messages on messages page

---
+++ app/Livewire/AllChatConversations.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\DB;
+use Musonza\Chat\Facades\ChatFacade as Chat;
+
+class AllChatConversations extends Component
+{
+    public $conversations;
+    public $display_user;
+    public $last_chat_message;
+
+    public function mount(): void
+    {
+        $this->refreshComponent();
+    }
+    
+    public function refreshComponent(): void
+    {
+        // // Retrieve the user
+        $user = \App\Models\Person::find(auth()->user()->id);
+
+        $all_conversations = Chat::conversations()->setParticipant($user)
+                                ->get()
+                                ->toArray()['data'];
+
+        $i = 0;
+        foreach ($all_conversations as $conversation) {
+            if ($conversation['conversation']['participants'][0]['messageable']['id'] !== auth()->user()->id) {
+                $display_user = $conversation['conversation']['participants'][0]['messageable'];
+            } else {
+                $display_user = $conversation['conversation']['participants'][1]['messageable'];
+            }
+
+            $last_messages = DB::table('chat_messages')
+                                    ->where('conversation_id', $conversation['conversation_id'])
+                                    ->whereNot('participation_id', auth()->user()->id)
+                                    ->get();
+
+            $last_message = $last_messages[count($last_messages) - 1];
+            $this->conversations[$i]['display_user'] = $display_user;
+            $this->conversations[$i]['last_message'] = $last_message;
+
+            $i++;
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.all-chat-conversations');
+    }
+}
---
+++ resources/views/livewire/all-chat-conversations.blade.php
@@ -0,0 +1,33 @@
+<div class="col-lg-4 col-md-12 no-pdd">
+    <div class="msgs-list">
+        <div class="msg-title">
+            <h3>Messages</h3>
+            <ul>
+                <li><a href="#" title=""><i class="fa fa-cog"></i></a></li>
+                <li><a href="#" title=""><i class="fa fa-ellipsis-v"></i></a></li>
+            </ul>
+        </div><!--msg-title end-->
+        <div class="messages-list">
+            <ul>
+                @foreach ($conversations as $conversation)
+                    <a href="{{ route('messages.index', $conversation['last_message']->conversation_id) }}">
+                        <li class="active">
+                            <div class="usr-msg-details">
+                                <div class="usr-ms-img">
+                                    <img src="{{ asset($conversation['display_user']['profile_image']) }}" alt="Profile image">
+                                    <span class="msg-status"></span>
+                                </div>
+                                <div class="usr-mg-info">
+                                    <h3>{{ $conversation['display_user']['full_name'] }}</h3>
+                                    <p>{{ $conversation['last_message']->body }} <img src="images/smley.png" alt=""></p>
+                                </div><!--usr-mg-info end-->
+                                <span class="posted_time">{{ \Carbon\Carbon::parse($conversation['last_message']->created_at)->diffForHumans() }}</span>
+                                <span class="msg-notifc">1</span>
+                            </div><!--usr-msg-details end-->
+                        </li>
+                    </a>
+                @endforeach
+            </ul>
+        </div><!--messages-list end-->
+    </div><!--msgs-list end-->
+</div>
